6a147d68b8c3e18f4b1b7aef1001825c8e3495a0
nchabanoles <nchabanoles@users.noreply.github.com>
2023-08-02 15:58:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,26 @@
-import streamlit as st
-import pandas as pd
-from langchain.chat_models import ChatOpenAI
-from langchain.agents import create_pandas_dataframe_agent
-from langchain.agents.agent_types import AgentType
+# First
+import openai import streamlit as st
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-# Page title
-st.set_page_config(page_title='🦜🔗 Ask the Data App')
-st.title('🦜🔗 Ask the Data App')
+st.title("💬 Chatbot") if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-# Load CSV file
-def load_csv(input_csv):
-  df = pd.read_csv(input_csv)
-  with st.expander('See DataFrame'):
-    st.write(df)
-  return df
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
 
-# Generate LLM response
-def generate_response(csv_file, input_query):
-  llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.2, openai_api_key=openai_api_key)
-  df = load_csv(csv_file)
-  # Create Pandas DataFrame Agent
-  agent = create_pandas_dataframe_agent(llm, df, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS)
-  # Perform Query using the Agent
-  response = agent.run(input_query)
-  return st.success(response)
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
 
-# Input widgets
-uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
-question_list = [
-  'How many rows are there?',
-  'What is the range of values for MolWt with logS greater than 0?',
-  'How many rows have MolLogP value greater than 0.',
-  'Other']
-query_text = st.selectbox('Select an example query:', question_list, disabled=not uploaded_file)
-openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
-
-# App logic
-if query_text is 'Other':
-  query_text = st.text_input('Enter your query:', placeholder = 'Enter query here ...', disabled=not uploaded_file)
-if not openai_api_key.startswith('sk-'):
-  st.warning('Please enter your OpenAI API key!', icon='⚠')
-if openai_api_key.startswith('sk-') and (uploaded_file is not None):
-  st.header('Output')
-  generate_response(uploaded_file, query_text)
+    openai.api_key = openai_api_key
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
