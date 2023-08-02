b87b5fdb62f8139ea47595f3003e148d1fd9585e
nchabanoles <nchabanoles@users.noreply.github.com>
2023-08-02 15:50:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,22 @@
 import streamlit as st
 from langchain.llms import OpenAI
 
-st.title('🦜🔗 My first AI App')
+st.title("🦜🔗 Langchain Quickstart App")
 
-openai_api_key = st.sidebar.text_input('OpenAI API Key')
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+
 
 def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-  st.info(llm(input_text))
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    st.info(llm(input_text))
 
-with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-  submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text)
+
+with st.form("my_form"):
+    text = st.text_area("Enter text:", "What are 3 key advice for learning how to code?")
+    submitted = st.form_submit_button("Submit")
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+    elif submitted:
+        generate_response(text)
